c56f5474493506904fc72a06c0203c9d31961ecb
Josh Nothum <joshnothum@gmail.com>
2018-02-27 03:42:44 -0600

    removed mongod and added pg

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+
 
 app.listen(port, function () {
     console.log('thx for listening on station:', port);
-    
 }); //the port you want to use
