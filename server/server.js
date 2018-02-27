3fa5e00cbf6950c4edc905f338416406b198776e
Josh Nothum <joshnothum@gmail.com>
2018-02-27 04:11:22 -0600

    git push -u origin master

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const promise = require('bluebird');
-const pgp = require('pg-promise');
-const connectionString = 'postgres://localhost:3000/puppies';
+
+
 
 app.listen(port, function () {
     console.log('thx for listening on station:', port);
