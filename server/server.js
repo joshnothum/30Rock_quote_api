afc1405e3f573637e43c77a672b3063e929df963
Josh Nothum <joshnothum@gmail.com>
2018-02-27 04:24:44 -0600

    connected database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const pg = require('pg');
+const port = process.env.PORT || 5000;
 
+const config = {
+    database: 'thirtyRock', // the name of the database
+    host: 'localhost', // where is your database?
+    port: 5432, // the port number for you database, 5432 is the default
+    max: 10, // how many connections at one time
+    idleTimeoutMillis: 30000 // Close idle connections to db after
+};
 
+const pool = new pg.Pool(config);
 
 app.listen(port, function () {
     console.log('thx for listening on station:', port);
