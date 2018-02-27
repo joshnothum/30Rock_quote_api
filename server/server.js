b657f0fb88c965a134893efc69d6164524d81467
Josh Nothum <joshnothum@gmail.com>
2018-02-27 04:41:58 -0600

    added route and database

--- server/server.js
+++ server/server.js
@@ -11,8 +11,31 @@ const config = {
     max: 10, // how many connections at one time
     idleTimeoutMillis: 30000 // Close idle connections to db after
 };
-
 const pool = new pg.Pool(config);
+
+app.get('/quotes',(req, res) => {
+    pool.connect(function (errorConnectingToDb, db, done) {
+        if (errorConnectingToDb) {
+            // There was an error and no connection was made
+            console.log('Error connecting', errorConnectingToDb);
+        } else {
+            // We connected to the db!!!!! pool -1
+            let queryText = 'SELECT *'+
+                            'FROM "quotes';
+            db.query(queryText, [], function (errorMakingQuery, result) {
+                // We have received an error or result at this point
+                done(); // pool +1
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                } else {
+                    // Send back success!
+                    console.log("RESULT: ", result.rows);
+                   res.send(result.rows);
+                }
+            }); // END QUERY
+        }
+    }); // END POOL
+});
 
 app.listen(port, function () {
     console.log('thx for listening on station:', port);
